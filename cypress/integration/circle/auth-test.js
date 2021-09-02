a5c20c43dfb47141f1c49be0a2463c690fc35e89
“Apoorv <tiwari.apoorv1316@gmail.com>
2021-09-02 12:04:37 +0530

    refactor auth tests

--- cypress/integration/circle/auth-test.js
+++ cypress/integration/circle/auth-test.js
@@ -1,52 +1,53 @@
 /// <reference types="cypress" />
 describe("user authentication", () => {
-  it("login page works fine", () => {
-    cy.visit("https://circle.so/");
+  beforeEach(() => {
+    cy.fixture("credentials").as("data");
+  });
+
+  it("should take user to login page", () => {
+    cy.visit("/");
     cy.get(".link-login").click();
     cy.get('[data-action="click->toggle#toggle click->signin#focus"]').click();
     cy.get("h1").should("have.text", "Log in to your Circle account");
   });
 
-  it("display incorrect email or password when credentials incorrect", () => {
-    cy.visit("https://login.circle.so/sign_in#email");
-
-    // Error does not exist at this point
-    cy.contains("Oops! Invalid email or password").should("not.exist");
-
-    cy.get('[data-target="signin.email"]').type("hello@gmail.com");
-    cy.get("#user_password").type("password");
-    cy.get('[data-disable-with="Log in"]').click();
-    cy.contains("Oops! Invalid email or password");
+  it("should display incorrect email or password when credentials incorrect", () => {
+    cy.get("@data").then((data) => {
+      // Error does not exist at this point
+      cy.contains(data.invalidCredentialsMessage).should("not.exist");
+      cy.login(data.invalidEmail, data.invalidPassword);
+      cy.contains(data.invalidCredentialsMessage);
+    });
   });
 
   it("should sign in when correct credentials", () => {
-    cy.visit("https://login.circle.so/sign_in#email");
-    cy.get('[data-target="signin.email"]').type("tiwari.apoorv1316@gmail.com");
-    cy.get("#user_password").type("Circle@123");
-    cy.get('[data-disable-with="Log in"]').click();
+    cy.get("@data").then((data) => {
+      cy.login(data.correctEmail, data.correctPassword);
+    });
   });
 
-  it("display error if email not found for forgot password", () => {
-    cy.visit("https://login.circle.so/users/password/new");
-    // warning doesn't exist at this point
-    cy.get(".alert-warning").should("not.exist");
+  it("should display error if email not found for forgot password", () => {
+    cy.visit(Cypress.env("forgotPasswordUrl"));
+    cy.get("@data").then((data) => {
+      // warning doesn't exist at this point
+      cy.get(".alert-warning").should("not.exist");
 
-    cy.get("#user_email").type("woooowooo@gmail.com");
-    cy.get('[data-disable-with="Reset my password"]').click();
-    cy.get(".alert-warning").should("have.text", "not found");
+      cy.get("#user_email").type(data.incorrectEmail);
+      cy.get('[data-disable-with="Reset my password"]').click();
+      cy.get(".alert-warning").should("have.text", "not found");
+    });
   });
 
   it("should send reset password confirmation with correct email", () => {
-    cy.visit("https://login.circle.so/users/password/new");
+    cy.visit(Cypress.env("forgotPasswordUrl"));
     // Message not exist at this point
-    cy.contains(
-      "You will receive an email with instructions on how to reset your password in a few minutes."
-    ).should("not.exist");
-    cy.get("#user_email").type("tiwari.apoorv1316@gmail.com");
-    cy.get('[data-disable-with="Reset my password"]').click();
-    cy.location("pathname").should("eq", "/sign_in");
-    cy.contains(
-      "You will receive an email with instructions on how to reset your password in a few minutes."
-    ).should("exist");
+
+    cy.get("@data").then((data) => {
+      cy.contains(data.emailConfirmationMessage).should("not.exist");
+      cy.get("#user_email").type(data.correctEmail);
+      cy.get('[data-disable-with="Reset my password"]').click();
+      cy.location("pathname").should("eq", "/sign_in");
+      cy.contains(data.emailConfirmationMessage).should("exist");
+    });
   });
 });
