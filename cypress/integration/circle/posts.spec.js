54cb4895b61f34bd8e3a93f55bbceb43723ceb03
“Apoorv <tiwari.apoorv1316@gmail.com>
2021-09-14 11:46:17 +0530

    test for creating post with a space

--- cypress/integration/circle/posts.spec.js
+++ cypress/integration/circle/posts.spec.js
@@ -7,9 +7,25 @@ describe("dasboard", () => {
       cy.login(data.correctEmail, data.correctPassword);
     });
   });
-  it("should create a post", () => {
+  it("should not create a post without a space", () => {
     cy.get("h2").should("be.visible");
     cy.get("#sidebar--right__btn-quick-post").click();
+    // No error at this stage
+    cy.contains("Please select a space.").should("not.exist");
     cy.get('[data-tribute="true"]').type("hello world!!");
+    cy.get(".quickpost-modal__footer--actions").within(() => {
+      cy.get("button").click();
+    });
+    cy.get(".form-errors").should("have.text", "Please select a space.");
+  });
+
+  it.only("should create a post with a space", () => {
+    cy.get("#sidebar--right__btn-quick-post").click();
+    cy.get(".react-space-selector__space-name").click();
+    cy.get('[data-tribute="true"]').type("hello world!!");
+    cy.get(".react-space-selector-dropdown__name").click({ force: true });
+    cy.get(".quickpost-modal__footer--actions").within(() => {
+      cy.get("button").click();
+    });
   });
 });
